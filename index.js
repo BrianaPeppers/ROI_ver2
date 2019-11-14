c5f54eb87ba30520db887eaeee89dba7f8c3bac5
Briana Peppers <briana.peppers@ghtechlabs.com>
2019-11-14 14:00:10 -0500

    inputs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // the inputs will be cleared after page navigation/refresh
 //to prevent that everything will be stored in cookies
 //and then calculated at the end on the results page
+//Storing Values
+
 var ROI = {
     time: 0,
     dollars: 0,
@@ -8,21 +10,34 @@ var ROI = {
     clients: 0
 }
 
+//Storing the values
 
 
 //Collecting Time
 function time(){
     var hours = [$('#timeQ1').val(), $('#timeQ2').val(), $('#timeQ3').val(), $('#timeQ4').val()];
-    //sum values
+    // ROI = update(sessionStorage.getItem('inputs'));
     ROI.time = hours.reduce((a,b) => parseInt(a)+parseInt(b),0);
     sessionStorage.setItem("inputs", JSON.stringify(ROI));
-  
+    
 }
 //Collecting Money
 function money(){
     var dollars = [$('#moneyQ1').val(), $('#moneyQ2').val(), $('#moneyQ3').val()];
-    ROI.money = dollars.reduce((a,b) => parseInt(a)+parseInt(b),0);
+    ROI = update(sessionStorage.getItem('inputs'));
+    ROI.dollars = dollars.reduce((a,b) => parseInt(a)+parseInt(b),0); 
     sessionStorage.setItem("inputs", JSON.stringify(ROI));
 }
 
 // CDR
+function costDollarRaised(){
+    var cdr =( $('#CDRexpenses').text()/$('#CDRrevenue').text());
+    ROI = update(sessionStorage.getItem('inputs'));
+    ROI.cdr = cdr;
+    sessionStorage.setItem("inputs", JSON.stringify(ROI));
+}
+
+// update ROI
+function update(ROI){
+    return JSON.parse(ROI);
+}
